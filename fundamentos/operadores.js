86a241b068c26aaa531c47c40a27d4751e6d13a3
Samuelhigino <146750376+samuelhigino940@users.noreply.github.com>
2025-01-22 17:24:24 -0300

    Add files via upload

---
+++ fundamentos/operadores.js
@@ -0,0 +1,38 @@
+/**
+ * OPERADORES
+ * -Aritméticos
+ * - Aritméticos de atribuição
+ * - Comparação
+ * -Logicos
+ */
+
+console.clear()
+console.log("Operadores aritméticos")
+console.log("+ | soma")
+console.log("- | subtração")
+console.log("* | multiplicação")
+console.log("/ | divisão")
+console.log("% | Modulo(Resto da divisão)")
+console.log("")
+console.log("Operadores aritméticos de atribuição")
+console.log("+= | soma e atribuição a variavel")
+console.log("-= | subtração e atribuição a variavel")
+console.log("*= | multiplicação e atribuição a variavel")
+console.log("/= | divisão e atribuição a variavel")
+console.log("++| somar 1 ao resultado")
+console.log("--| subtrair 1 ao resultado")
+console.log("")
+console.log("Comparadores")
+console.log("> | maior que")
+console.log("< | menor que")
+console.log(">= | maior ou igual a ")
+console.log("<= | menor ou igual a")
+console.log("=== | idêntico a")
+console.log("!== | não idêntico a")
+console.log("")
+console.log("Operadores Lógicos")
+console.log("&& | AMD")
+console.log("|| | OR")
+console.log("!  | NOT")
+
+
---
+++ html/script.js
@@ -0,0 +1,11 @@
+/**
+ * HTML - JS
+ */
+
+console.log("hello HTML - JS linkado")
+
+function hello() {
+    let nome
+    nome = prompt("Qual é o sue nome?")
+    alert(`hello ${nome}`)
+}
